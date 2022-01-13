267612e25ed6d667dceb066426e39e330c7b8567
Margarita Mayoral Villa <margaritamayoralvilla@Margaritas-MacBook-Pro.local>
2022-01-13 13:00:53 -0500

    some changes in the code  to get better accuracy

--- src/keyword_class.py
+++ src/keyword_class.py
@@ -96,74 +96,74 @@ nlp.add_pipe(language_detector)
 
 ##### Loading the data ####
 
-df = pd.read_json('../../keyword_categories/keyword_categories_train.jsonl', lines=True)
-df_test = pd.read_json('../../keyword_categories/keyword_categories_test.jsonl', lines=True)
-
-###### Data training exploration ##################
-#looking the training data
-print(df.head(10))
-#looking for missings, kind of data and shape:
-print(df.info())
-###### Data Cleaning and Normalization ####
-print(df['categories'].head(10))
-df = df.explode('categories')
-print(df['categories'].head(10))
-print(df.head(10))
-print(df.info())
-
-keywords = df['keyword']
-print("detecting languages in training data")
-languages_langdetect = []
-for line in keywords:
-    try:
-        result = langdetect.detect_langs(line)
-        #print(result)
-        result = str(result[0])[:2]
-        #print(result)
-    except:
-        result = 'unknown'
-    
-    finally:
-        languages_langdetect.append(result)
-df['languages_langdetect'] = languages_langdetect
-print(df.head(10))
-## dropping the rows which contains languages that are not english  ###
-df.drop(df[df['languages_langdetect'] != 'en'].index, inplace = True)
-print(df.head(10))
-print(df.info())
+#df = pd.read_json('../../keyword_categories/keyword_categories_train.jsonl', lines=True)
+#df_test = pd.read_json('../../keyword_categories/keyword_categories_test.jsonl', lines=True)
 #
-## saving in csv ##
-df.to_csv('../data/keyword_categories_train_clean.csv')
+####### Data training exploration ##################
+##looking the training data
+#print(df.head(10))
+##looking for missings, kind of data and shape:
+#print(df.info())
+####### Data Cleaning and Normalization ####
+#print(df['categories'].head(10))
+#df = df.explode('categories')
+#print(df['categories'].head(10))
+#print(df.head(10))
+#print(df.info())
 #
-#########  Data test exploration ###########
-print(df_test.head(10))
-print(df_test.info())
-df_test = df_test.explode('categories')
-print(df_test['categories'].head(10))
-print(df_test.head())
-print(df_test.info())
-keywords_test = df_test['keyword']
-print("detecting languages 2")
-languages_langdetect_test = []
-for line in keywords_test:
-    try:
-        result = langdetect.detect_langs(line)
-        #print(result)
-        result = str(result[0])[:2]
-        #print(result)
-    except:
-        result = 'unknown'
-    
-    finally:
-        languages_langdetect_test.append(result)
-df_test['languages_langdetect'] = languages_langdetect_test
-print(df_test.head(10))
-## dropping the rows which contains languages that are not english  ###
-df_test.drop(df_test[df_test['languages_langdetect'] != 'en'].index, inplace = True)
-print(df_test.head(10))
-print(df_test.info())
-## saving in csv ##
-df.to_csv('../data/keyword_categories_test_clean.csv')
+#keywords = df['keyword']
+#print("detecting languages in training data")
+#languages_langdetect = []
+#for line in keywords:
+#    try:
+#        result = langdetect.detect_langs(line)
+#        #print(result)
+#        result = str(result[0])[:2]
+#        #print(result)
+#    except:
+#        result = 'unknown'
+#
+#    finally:
+#        languages_langdetect.append(result)
+#df['languages_langdetect'] = languages_langdetect
+#print(df.head(10))
+### dropping the rows which contains languages that are not english  ###
+#df.drop(df[df['languages_langdetect'] != 'en'].index, inplace = True)
+#print(df.head(10))
+#print(df.info())
+##
+### saving in csv ##
+#df.to_csv('../data/keyword_categories_train_clean.csv')
+##
+##########  Data test exploration ###########
+#print(df_test.head(10))
+#print(df_test.info())
+#df_test = df_test.explode('categories')
+#print(df_test['categories'].head(10))
+#print(df_test.head())
+#print(df_test.info())
+#keywords_test = df_test['keyword']
+#print("detecting languages 2")
+#languages_langdetect_test = []
+#for line in keywords_test:
+#    try:
+#        result = langdetect.detect_langs(line)
+#        #print(result)
+#        result = str(result[0])[:2]
+#        #print(result)
+#    except:
+#        result = 'unknown'
+#
+#    finally:
+#        languages_langdetect_test.append(result)
+#df_test['languages_langdetect'] = languages_langdetect_test
+#print(df_test.head(10))
+### dropping the rows which contains languages that are not english  ###
+#df_test.drop(df_test[df_test['languages_langdetect'] != 'en'].index, inplace = True)
+#print(df_test.head(10))
+#print(df_test.info())
+### saving in csv ##
+#df.to_csv('../data/keyword_categories_test_clean.csv')
 #
 #
 #
@@ -176,24 +176,31 @@ print(df_test.head(10))
 counter = Counter(df['categories'].tolist())
 print(counter)
 #
+top_10_categories = {i[0]: idx for idx, i in enumerate(counter.most_common(10))}
+df = df[df['categories'].map(lambda x: x in top_10_categories)]
+print(df.head())
 keyword_list = df['keyword'].tolist()
-categories_list = df['categories'].tolist()
+categories_list = [top_10_categories[i] for i in df['categories'].tolist()]
 print(categories_list)
 categories_list = np.array(categories_list)
+print(categories_list)
 
-count_vect = CountVectorizer()
+count_vect = CountVectorizer(stop_words='english')
 x_train_counts = count_vect.fit_transform(keyword_list)
 tfidf_transformer = TfidfTransformer()
 x_train_tfidf = tfidf_transformer.fit_transform(x_train_counts)
 
 ## test data ##
-counter = Counter(df_test['categories'].tolist())
-print(counter)
+counter_test = Counter(df_test['categories'].tolist())
+print(counter_test)
+#top_10_categories_test = {i[0]: idx for idx, i in enumerate(counter_test.most_common(10))}
+df_test = df_test[df_test['categories'].map(lambda x: x in top_10_categories)]
+print(df_test.head())
 keyword_list_test = df_test['keyword'].tolist()
-categories_list_test = df_test['categories'].tolist()
+categories_list_test = [top_10_categories[i] for i in df_test['categories'].tolist()]
 print(categories_list_test)
 categories_list_test = np.array(categories_list_test)
-
+print(categories_list_test)
 x_test_counts = count_vect.fit_transform(keyword_list_test)
 x_test_tfidf = tfidf_transformer.fit_transform(x_test_counts)
 
@@ -210,7 +217,7 @@ clf = MultinomialNB().fit(train_x, train_y)
 print(clf)
 #
 y_score = clf.predict(test_x)
-print(y_score)
+print(test_x, y_score)
 print("the NB y score is:", y_score)
 #
 #text_clf = Pipeline([
@@ -219,7 +226,7 @@ print("the NB y score is:", y_score)
 #('clf', MultinomialNB()),
 #])
 ##
-#text_clf = text_clf.fit(train_x, train_y)
+
 
 
 #
@@ -231,61 +238,57 @@ for i in range(len(y_score)):
 print("Accuracy: %.2f%%" % ((n_right/float(len(test_y)) * 100)))
 #
 ##
-###### It gave an accuracy of about  ###
+###### It gave an accuracy of about  36.43%###
 #
 ######################################
 ##### Support Vector Machines (SVM) ###
-#clf_svm_1 = SVC(kernel='linear').fit(train_x, train_y)
-#y_score_svm_1 = clf_svm_1.predict(test_x)
 ##
-#n_right_svm_1 = 0
-#for i in range(len(y_score_svm_1)):
-#    if y_score_svm_1[i] == test_y[i]:
-#        n_right_svm_1 += 1
+print("computing SGD Classifier")
+clf_svm_2 = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42).fit(train_x, train_y)
+y_score_svm_2 = clf_svm_2.predict(test_x)
+print(y_score_svm_2)
 ##
-#print("Accuracy: %.2f%%" % ((n_right_svm_1/float(len(test_y)) * 100)))
+n_right_svm_2 = 0
+for i in range(len(y_score_svm_2)):
+    if y_score_svm_2[i] == test_y[i]:
+        n_right_svm_2 += 1
 ##
-#clf_svm_2 = SGDClassifier(loss='hinge', penalty='l2',alpha=1e-3, random_state=42).fit(train_x, train_y)
-#y_score_svm_2 = clf_svm_2.predict(test_x)
-##
-#n_right_svm_2 = 0
-#for i in range(len(y_score_svm_2)):
-#    if y_score_svm_2[i] == test_y[i]:
-#        n_right_svm_2 += 1
-##
-#print("Accuracy: %.2f%%" % ((n_right_svm_2/float(len(test_y)) * 100)))
+print("SGD Classifier Accuracy: %.2f%%" % ((n_right_svm_2/float(len(test_y)) * 100)))
+
+###### It gave an accuracy of about  38.71%###
 #
 ### Deep learning model  ###
+print("Calculating Deep Learning model")
+mapped_list, word_list = filter_to_top_x(keyword_list, 2500, 10)
 
-#mapped_list, word_list = filter_to_top_x(description_list, 2500, 10)
-#varietal_list_o = [top_10_varieties[i] for i in df['variety'].tolist()]
-#varietal_list = to_categorical(varietal_list_o)
+categories_list_o = [top_10_categories[i] for i in df['categories'].tolist()]
+print(categories_list_o)
+categories_list = to_categorical(categories_list_o)
 #
-#max_review_length = 150
+max_keyword_length = 150
 #
-#mapped_list = sequence.pad_sequences(mapped_list, maxlen=max_review_length)
-#train_x, test_x, train_y, test_y = train_test_split(mapped_list, varietal_list, test_size=0.3)
+mapped_list = sequence.pad_sequences(mapped_list, maxlen=max_keyword_length)
+#train_x, test_x, train_y, test_y = train_test_split(mapped_list, categories_list, test_size=0.3)
 #
-#max_review_length = 150
+max_keyword_length = 150
+embedding_vector_length = 64
+print("training DL model ...")
+model = Sequential()
 #
-#embedding_vector_length = 64
-#model = Sequential()
+model.add(Embedding(2500, embedding_vector_length, input_length=max_keyword_length))
+model.add(Conv1D(50,5))
+model.add(Flatten())
+model.add(Dense(100, activation='relu'))
+model.add(Dense(max(categories_list_o) + 1, activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+model.fit(train_x, train_y, epochs=3, batch_size=64)
 #
-#model.add(Embedding(2500, embedding_vector_length, input_length=max_review_length))
-#model.add(Conv1D(50,5))
-#model.add(Flatten())
-#model.add(Dense(100, activation='relu'))
-#model.add(Dense(max(varietal_list_o) + 1, activation='softmax'))
-#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-#model.fit(train_x, train_y, epochs=3, batch_size=64)
-#
-#y_score = model.predict(test_x)
-#y_score = [[1 if i == max(sc) else 0 for i in sc] for sc in y_score]
-#n_right = 0
-#for i in range(len(y_score)):
-#    if all(y_score[i][j] == test_y[i][j] for j in range(len(y_score[i]))):
-#        n_right += 1
-#print("Accuracy: %.2f%%" % ((n_right/float(len(test_y)) * 100)))
-#print(clf_svm)
+y_score = model.predict(test_x)
+y_score = [[1 if i == max(sc) else 0 for i in sc] for sc in y_score]
+n_right = 0
+for i in range(len(y_score)):
+    if all(y_score[i][j] == test_y[i][j] for j in range(len(y_score[i]))):
+        n_right += 1
+print("Accuracy DL: %.2f%%" % ((n_right/float(len(test_y)) * 100)))
 
 
